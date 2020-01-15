1407cbb203d7c615edac6addd6356fb898da4ea1
Winston  Chung <18.chung.winston@ctkprep.org>
2020-01-15 22:21:51 +0000

    Automatically backed up by Learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
 end
-describe Student do
+RSpecdescribe Student do
   before(:each) do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
   end
