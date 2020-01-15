e2a4a99ac47a52586dab77bd9fdad2df48894ead
Winston  Chung <18.chung.winston@ctkprep.org>
2020-01-15 22:22:33 +0000

    Automatically backed up by Learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Student, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-end
-RSpecdescribe Student do
   before(:each) do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
   end
@@ -16,3 +14,16 @@ RSpecdescribe Student do
     expect(@student.to_s).to eq("Daenerys Targaryen")
   end
 end
+#RSpecdescribe Student do
+ # before(:each) do
+ #   @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+#  end
+
+ # it 'can be created' do
+ #   expect(@student).to be_valid
+ # end
+
+ # it 'has a to_s instance method' do
+ #   expect(@student.to_s).to eq("Daenerys Targaryen")
+ # end
+#end
